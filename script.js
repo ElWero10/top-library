206ee51167355e70feb11dbd8d96821963417460
Wero <wero@bluecollarlabs.io>
2025-08-31 19:54:44 -0700

    Add JS and start working on css
    JS seems to work adding books and displaying
    Working on Grid Layout

---
+++ script.js
@@ -0,0 +1,49 @@
+const container = document.querySelector(".container");
+const newBookBtn = document.querySelector("button");
+
+const myLibrary = [
+    {
+        title: "Harry Potter",
+        author: "JK Rowling",
+        pages: "300"
+    },
+    {
+        title: "Lord of the Rings",
+        author: "J.R.R Tolkien",
+        pages: "600"
+    }
+];
+
+function Book(title, author, pages) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages
+}
+
+function addBookToLibrary(title, author, pages) {
+    const book = new Book(title, author, pages);
+    book.id = crypto.randomUUID();
+    myLibrary.push(book);
+    displayLibrary(myLibrary);
+}
+
+function displayLibrary(library) {
+    for(let i = 0; i < library.length; i++) {
+        const book = library[i];
+        const card = document.createElement("div");
+        card.classList.add(".card")
+        card.textContent = `${book.title} 
+        by ${book.author} is ${book.pages} pages long.`
+        container.appendChild(card);
+    }
+}
+
+newBookBtn.addEventListener("click", () => {
+    const title = prompt("Enter a book");
+    const author = prompt("Enter the author");
+    const pages = prompt("Enter the page length");
+
+    addBookToLibrary(title, author, pages);
+})
+
+displayLibrary(myLibrary)
